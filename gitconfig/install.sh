37e2dbc0734dd14fe99a7d6ca37bc89fdab3a3e9
pranavb <pranavb@zillowgroup.com>
2021-03-11 15:44:20 -0800

    remove email

--- gitconfig/install.sh
+++ gitconfig/install.sh
@@ -1,3 +1,7 @@
+
+git config --global user.name pranavb
+git config --global user.email
+
 git config --global push.default current
 git config --global status.short true
 git config --global status.branch true
